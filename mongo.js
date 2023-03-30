a4a4af24a054a8905ed34a480a8a4c72e0d291e6
Luis Esneyder Fernández <luisexneider1999@gmail.com>
2023-03-30 15:56:08 -0500

    ejercicio 3.12

---
+++ mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3 ) {
+    console.log('give password or data as argument');
+    process.exit(1)
+}
+const password = process.argv[2]
+
+const url = `mongodb+srv://luisexneider1999:${password}@persons.dqlvflx.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = mongoose.Schema({
+    name: String,
+    number: String,
+}) 
+const Persons = mongoose.model('Person', personSchema)
+
+// const person = new Persons({
+//     name: process.argv[3],
+//     number: process.argv[4]
+// })
+
+// person.save().then(result => {
+//     console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`);
+//     mongoose.connection.close()
+// })
+console.log('Phonebook:');
+Persons.find({}).then(result => {
+    result.forEach(element => {
+        console.log(`${element.name} ${element.number}`);
+    })
+    mongoose.connection.close()
+})
