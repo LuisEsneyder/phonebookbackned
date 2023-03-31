c6c738d425187555f7480710e4c40c68835939f2
Luis Esneyder Fernández <luisexneider1999@gmail.com>
2023-03-31 13:33:49 -0500

    ejercicio 3.15 y 3.16

--- index.js
+++ index.js
@@ -10,39 +10,22 @@ const app = express()
 morgan.token('datos', function getDatos(req){
     return JSON.stringify(req.body)
 })
+//se crea el middlewere para el controlador de error
+const errorHandle = (error, request, response, next) => {
+    console.error(error.message);
+    if(error.name === 'CastError'){
+        return response.status(400).send({error: 'malformatted id'})
+    }
+    next(error)
+}
 //se agregan los middlewere al app
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())//se utiliza el metodo para leer los datos en formato json 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :datos'))
 //datos
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-//Función generador de id
-const generarId = () => {
-    const numberRandon = Math.random()*1000
-    return Math.floor(numberRandon)
-}
+
+
 //se crea la ruta para obtener todos los datos
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(result => {
@@ -72,10 +55,10 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
 })
 //se crea ruta para eliminar a una persona
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(element => element.id !== id)
-    response.status(202).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id).then(result => {
+        response.status(200).end()
+    }).catch(error => next(error))
 })
 //se crea una ruta para crear una persona en el phonebook
 app.post(('/api/persons'), (request, response) => {
@@ -97,6 +80,8 @@ app.post(('/api/persons'), (request, response) => {
         response.status(200).json(savedPerson)
     })
 })
+//se usa el middlewere de error
+app.use(errorHandle)
 //se pone el servidor a escuchar las peticiones por un puerto dado
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
